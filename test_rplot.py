bdb71a23fed5f6bbd155bb63900d75fef2a72217
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2015-03-01 15:00:45 +0100

    Rewrite test_rplot using unittest and file hash

--- test_rplot.py
+++ test_rplot.py
@@ -1,60 +1,108 @@
 #!/usr/bin/env python
 
+import unittest
+import os
+import hashlib
 from fixes import ROOT
-ROOT.gROOT.SetBatch(True)
-
-## Test overlaying with transparency
-hists = [ROOT.TH1F('hist{}'.format(i), '', 100, -10, 10) for i in xrange(16)]
-for i, hist in enumerate(hists):
-    fn = ROOT.TF1('fn{}'.format(i), 'TMath::Gaus(x, {0}, 1)'.format(i-8), -10, 10)
-    hist.FillRandom('fn{}'.format(i), 1000)
-    hist.SetXTitle('Foo[bar]')
-    hist.SetYTitle('Events')
-    del fn
-
+from ROOT import TH1F, TF1
+from rplot import arrange, Rplot
 from string import ascii_lowercase
-histl = [ROOT.TH1F('hist{}'.format(l), '', 100, -10, 10) for l in ascii_lowercase[0:2]]
-for i, l in enumerate(ascii_lowercase[0:2]):
-    fn = ROOT.TF1('fn{}'.format(l), 'TMath::Gaus(x, {0}, 1)'.format(i), -10, 10)
-    histl[i].FillRandom('fn{}'.format(l), 1000)
-    histl[i].SetXTitle('Foo[bar]')
-    histl[i].SetYTitle('Events')
 
-histograms = [
-    [h for i, h in enumerate(hists) if i % 2 == 0],
-    [h for i, h in enumerate(hists) if i % 2 == 1],
-    histl[0], histl[1]
-]
+__pngfile__ = '/tmp/test.png'
 
-import rplot
 
-plotter = rplot.Rplot(2,2)
-plotter.draw_hist(histograms, 'hist')
+def setUpModule():
+    ROOT.gROOT.SetBatch(True)
+    ROOT.gErrorIgnoreLevel = ROOT.kWarning
 
-plotter.canvas.Update()
-plotter.canvas.Print('test.pdf[')
-plotter.canvas.Print('test.pdf')
-del plotter, histograms
 
-## Test stacking
-hists = [ROOT.TH1F('hist{}'.format(i), '', 100, -10, 10) for i in xrange(6)]
-for i, hist in enumerate(hists):
-    fn = ROOT.TF1('fn{}'.format(i), '{}*TMath::Gaus(x, 0, {})'.format(1./(i+1), i-3), -10, 10)
-    hist.FillRandom('fn{}'.format(i), 1000)
-    hist.SetXTitle('Foo[bar]')
-    hist.SetYTitle('Events')
-    del fn
+def tearDownModule():
+    if os.path.exists(__pngfile__):
+        os.remove(__pngfile__)
 
-histograms = [
-    [h for i, h in enumerate(hists) if i % 2 == 0],
-    [h for i, h in enumerate(hists) if i % 2 == 1],
-]
 
-plotter = rplot.Rplot(2,1)
-plotter.stack = True
-plotter.draw_hist(histograms, 'hist')
+# utilities
+def fill_hists(hists, fns):
+    """Fill histograms using given functions"""
+    for i, hist in enumerate(hists):
+        hist.FillRandom(fns[i].GetName(), 1000)
+        hist.SetXTitle('Foo[bar]')
+        hist.SetYTitle('Events')
 
-plotter.canvas.Update()
-plotter.canvas.Print('test.pdf')
-plotter.canvas.Print('test.pdf]')
-del plotter, histograms
+
+def file_hash(filename=__pngfile__):
+    """Calculate MD5 hash of file contents"""
+    with open(filename) as png:
+        contents = png.read()
+        return hashlib.md5(contents).hexdigest()
+
+
+class test_arrange(unittest.TestCase):
+    def setUp(self):
+        self.items = [[0, i] for i in xrange(16)]
+
+    def test_arrange(self):
+        rearranged = arrange(self.items, 8)
+        self.assertEqual(len(rearranged), 2)
+        self.assertEqual(len(rearranged[0]), len(rearranged[1]))
+
+    def test_reverse(self):
+        rearranged = arrange(self.items, 8)
+        map(lambda l: l.reverse(), rearranged)
+        items_r = arrange(self.items, 8, reverse=True)
+        map(self.assertListEqual, rearranged, items_r)
+
+    def test_predicate(self):
+        items_p = arrange(self.items, 8, predicate=lambda l: l.append(99))
+        ref = [99 for j in xrange(8)]
+        for i in xrange(2):
+            self.assertListEqual([items_p[i][j][-1] for j in xrange(8)], ref)
+
+
+class test_Rplot(unittest.TestCase):
+    """Test overlaying with transparency, and stacking"""
+
+    @classmethod
+    def setUpClass(self):
+        self.hists1 = [TH1F('hist{}'.format(i), '', 100, -10, 10)
+                       for i in xrange(16)]
+        fns = [TF1('fn{}'.format(i), 'TMath::Gaus(x, {}, 1)'.format(i-8),
+                   -10, 10) for i in xrange(len(self.hists1))]
+        fill_hists(self.hists1, fns)
+        del fns
+
+        self.hists2 = [TH1F('hist{}'.format(s), '', 100, -10, 10)
+                       for s in ascii_lowercase[0:6]]
+        fns = [TF1('fn{}'.format(i),
+                   '{}*TMath::Gaus(x, 0, {})'.format(1./(i+1), i-3), -10, 10)
+               for i in xrange(len(self.hists2))]
+        fill_hists(self.hists2, fns)
+        del fns
+
+        self.plots = [
+            [h for i, h in enumerate(self.hists1) if i % 2 == 0],
+            [h for i, h in enumerate(self.hists1) if i % 2 == 1],
+            [h for i, h in enumerate(self.hists2) if i % 2 == 0],
+            [h for i, h in enumerate(self.hists2) if i % 2 == 1],
+        ]
+
+    @classmethod
+    def tearDownClass(self):
+        del self.plots
+
+    def test_overlay(self):
+        plotter = Rplot(2, 2, 1200, 800)
+        plotter.draw_hist(self.plots, 'hist')
+        plotter.canvas.Update()
+        plotter.canvas.Print(__pngfile__)
+        self.assertEqual('25c6ff7ed5422880e47a0c50b0d6e044', file_hash())
+
+    def test_stack(self):
+        # splicing overwrites ROOT objects in memory
+        plots = [self.plots[2], self.plots[3]]
+        plotter = Rplot(2, 1, 1200, 400)
+        plotter.stack = True
+        plotter.draw_hist(plots, 'hist')
+        plotter.canvas.Update()
+        plotter.canvas.Print(__pngfile__)
+        self.assertEqual('df05887dc179fc0097d0f01dfd9f23a8', file_hash())
