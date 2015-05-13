9d11a4882f8134d288a96a233bf8e827e4de143e
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2015-05-13 12:44:53 +0200

    utils: Temporary ROOT file wrapper

--- utils.py
+++ utils.py
@@ -26,6 +26,37 @@ def root_str(objs):
     return res
 
 
+class Rtmpfile(object):
+    # NOTE: This is to ignore the security warning from os.tmpnam.
+    # There is no risk since I `recreate' the TFile.
+    from warnings import filterwarnings
+    filterwarnings(action='ignore', category=RuntimeWarning,
+                   message='tmpnam is a potential security risk.*')
+
+    def __init__(self, keep=False):
+        """Do not remove temporary file if `keep' is `True'"""
+        self.keep = keep
+        from os import tmpnam
+        from time import strftime, localtime
+        from ROOT import TFile
+        self.rfile = TFile.Open('{}-{}.root'.format(
+            tmpnam(), strftime('%y-%m-%d-%H%M%S%Z', localtime())), 'recreate')
+
+    def __del__(self):
+        from os import remove
+        self.rfile.Close()
+        if not self.keep:
+            remove(self.rfile.GetName())
+
+    def __enter__(self):
+        """Return the ROOT file when used with `with'"""
+        return self.rfile
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """Return True, in case an exception is raised, ignored otherwise."""
+        return True
+
+
 # RooFit utilities
 def dst_iter(dst):
     """RooAbsData iterator: generator to iterate over a RooDataSet"""
